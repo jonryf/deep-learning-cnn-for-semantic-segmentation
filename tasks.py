3a8b9e428624a428930590f880a0850d6ec61ac7
root <root@DESKTOP-OR6I109.localdomain>
2020-02-11 11:52:09 -0800

    added unet to tasks

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from runner import ModelRunner
 from basic_fcn import FCN
+from unet import UNET
 import torch
 import torch.nn as nn
 
@@ -62,6 +63,20 @@ def task_3_3():
 
     # combine two plots
     task_model.plot(baseline_runner, names=["Baseline", "Model with transformations"])
+
+def task_unet():
+    settings = {
+        'APPLY_TRANSFORMATIONS': False,
+        'model': UNET,
+        'EPOCHS': 100
+    }
+    print("Training UNET on", settings['EPOCHS'], "Epochs")
+    runner = ModelRunner(settings)
+    runner.load_data()
+    runner.train()
+    # runner.val()
+    runner.plot()
+
 
 if __name__ == "__main__":
     task2()
